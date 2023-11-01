ba024720c4c100dbee0d8db208d69df6bd3807cb
Joan Macharia <joan.macharia@students.moringa>
2023-11-01 23:11:55 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import NavBar from "./NavBar";
 
+import GiftListContainer from "./GifListContainer";
+
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GiftListContainer />
     </div>
   );
 }
